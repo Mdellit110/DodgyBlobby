d831bebf10ed590a9a071c635ac91451688da9fc
matthew dellitalia <matthewdellitalia@matthews-mbp-2.lan>
2019-02-03 15:24:31 -0500

    created function and event to control player movement

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+const body = document.querySelector('body');
+//player creation
 const createPlayer = () => {
   const player = document.querySelector('.player');
   player.style.background = 'blue';
   player.name = 'matt';
   player.setAttribute ('id','player');
+  player.style.left = '740px'
+  return player;
 }
+const chooseColor = () => {
+
+}
+const chooseName = () => {
+
+}
+
+//player movement
+let move = 740;
+const movePlayer = (ev) => {
+  const player = document.querySelector('.player');
+  if (ev.keyCode === 39 && move < 1490) { //rightArrow
+    move += 50;
+    player.style.left = `${move}px`;
+  } else if (ev.keyCode === 37 && move > 0) { //leftArrow
+    move -= 50;
+    player.style.left = `${move}px`;
+  }
+}
+
+body.addEventListener('keydown', movePlayer)
