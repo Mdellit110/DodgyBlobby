040d92791a0ae65211500fec77ce6a81c6bce82f
matthew dellitalia <matthewdellitalia@matthews-mbp-2.lan>
2019-02-08 02:42:19 -0500

    got rid of all intervals and set all animations to requestanimationframes. also implemented difficulty progression based on score

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // global variables
 const body = document.querySelector('body');
 const startButton = document.querySelector('#start');
-const tutorialButton = document.querySelector('#tutorial');
 const title = document.querySelector('.title')
 const player = document.querySelector('.player');
 const tryAgain = document.querySelector('.tryAgain');
@@ -10,19 +9,25 @@ const noButton = document.querySelector('#no');
 const finalScore = document.querySelector('.finalScore');
 let blocks = [];
 let score = 0;
+let gameTime;
 let move = 50; //initial player location when blobby appears
-let int; //name of dropEm interval
-let blockMaker; //name of generateBlocks interval
+let speed = 100; // speed of how blocks fall
+let playing = false;
 let left = false; // player moves left when true
 let right = false; // player moves right when true
+let rate = 11;// sets rate of generateBlocks
 
-//player movement
+
+//keybindings
+  //player movement
 const movePlayer = (ev) => { // initiates movements left/right
   if (ev.keyCode === 39 && player.offsetLeft <= (document.body.clientWidth - 10)) { //rightArrow
     right = true;
+    playing = true;
     player.classList.add('goRight')
   } else if (ev.keyCode === 37 && move > 1) { //leftArrow
     left = true;
+    playing = true;
     player.classList.add('goLeft')
   };
 };
@@ -35,7 +40,33 @@ const unMovePlayer = (ev) => { // stops movement left/right
     player.classList.remove('goLeft')
   };
 };
-const movingPlayer = () => { //determines movement speed and allows for pacman logic
+
+// generates new block at random distance off left wall
+const generateBlocks = () => {
+  if (playing && score % rate === 0) {
+    const block = document.createElement('div');
+    block.className = 'block'
+    block.style.left = `${randOffLeft(block)}px`; //random drop spot off left wall
+    blocks.push(block); //push new block into blocks array
+    body.appendChild(block);
+  };
+};
+
+// detects collision between BLOBBY and rocks
+const checkCollision = (i) => { //would like to refine this for a more accurate hit box
+  blockPos = blocks[i].getBoundingClientRect();
+  playerPos = player.getBoundingClientRect();
+  if (playerPos.bottom >= blockPos.bottom) {
+    if (playerPos.left + 2 >= blockPos.left && playerPos.right - 2 <= blockPos.right || playerPos.right - 2 >= blockPos.left && playerPos.left + 2 <= blockPos.right) {
+      if (playerPos.top + 1 <= blockPos.bottom) {
+        youLose();
+      };
+    };
+  };
+};
+
+// moves blobby
+const moveBlobby = () => {
   if (left){
     if (player.offsetLeft <= 0) { //if player hits left wall moves him to right wall
     move = 100;
@@ -47,49 +78,45 @@ const movingPlayer = () => { //determines movement speed and allows for pacman l
     }
     move += 1;
   };
-  player.style.left = `${move}%`; //moves player in 1% increments to adjust to screen size
-  window.requestAnimationFrame(movingPlayer);
-}
-
-// generates new block at random distance off left wall
-const generateBlocks = () => {
-  const block = document.createElement('div');
-  block.className = 'block'
-  block.style.left = `${randOffLeft(block)}px`; //random drop spot off left wall
-  blocks.push(block); //push new block into blocks array
-  body.appendChild(block);
+  player.style.left = `${move}%`;
 };
 
-// detects collision between BLOBBY and rocks
-checkCollision = (i) => { //would like to refine this for a more accurate hit box
-  blockPos = blocks[i].getBoundingClientRect();
-  playerPos = player.getBoundingClientRect();
-  if (playerPos.bottom >= blockPos.bottom) {
-    if (playerPos.left >= blockPos.left && playerPos.right <= blockPos.right || playerPos.right >= blockPos.left && playerPos.left <= blockPos.right) {
-      if (playerPos.top <= blockPos.bottom) {
-        clearInterval(int);
-        youLose();
+// drops the rocks
+const dropEm = () => {
+  if (blocks !== [] && playing) {
+    for (let i=0; i<blocks.length; i++) {
+      checkCollision(i);
+      if (blocks[i] !== undefined && (blocks[i].offsetTop) < (window.innerHeight - blocks[i].offsetHeight)) {
+        blocks[i].style.top =`${(blocks[i].offsetTop + (window.innerHeight / speed))}px`; // converts a percentage into pixel form to keep drop rate the same for all screen sizes
+      } else {
+        resetBlock();
       };
     };
   };
 };
 
-//functions to make blocks fall
-const dropEm = () => {
-  for (let i=0; i<blocks.length; i++) {
-    checkCollision(i);
-    if (blocks[i] !== undefined && (blocks[i].offsetTop) < (window.innerHeight - blocks[i].offsetHeight)) {
-        blocks[i].style.top =`${blocks[i].offsetTop + (window.innerHeight / 100)}px`; // converts a percentage into pixel form to keep drop rate the same for all screen sizes
-    } else {
-      resetBlock();
-    };
-  };
+// progression
+const speedUp = () => {
   score += 1;
+  if (score % 400 === 0) {
+    speed -= 5;
+  };
+  if (rate % 800 === 0) {
+    rate -= 1;
+  }
+};
+
+const movingParts = () => {
+  moveBlobby();
+  generateBlocks();
+  dropEm();
+  speedUp();
+  let gameTime = window.requestAnimationFrame(movingParts); //
 };
 
 // number generators
 const randOffLeft = () => { //tells blocks how wide the screen is so it wont generate new ones off the screen
-  return (Math.floor(Math.random() * (window.innerWidth - 40)));
+  return (Math.floor(Math.random() * (window.innerWidth - player.offsetWidth)));
 };
 
 // reset functions
@@ -107,25 +134,22 @@ const resetBlocks = () => {
 const startGame = () => { //when you hit start button on main page
   startButton.style.display = 'none';
   title.style.display = 'none';
-  tutorialButton.style.display = 'none';
   tryAgain.style.display = 'none';
   yesButton.style.display = 'none';
   noButton.style.display = 'none';
   player.style.display = 'block';
   finalScore.style.display = 'none';
-  blockMaker = setInterval(generateBlocks, 90);
-  int = setInterval(dropEm, 18);
   body.addEventListener('keydown', movePlayer, true);
   body.addEventListener('keyup', unMovePlayer, true);
 };
 const youLose = () => { // brings up try again when you get hit
+  playing = false;
   tryAgain.style.display = 'block';
   yesButton.style.display = 'block';
   noButton.style.display = 'block';
   finalScore.style.display = 'block';
   player.classList.add('dead')
   finalScore.innerText = `SCORE: ${Math.floor(score)}`;
-  clearInterval(blockMaker);
   body.removeEventListener('keydown', movePlayer, true);
   yesButton.addEventListener('click', restartGame);
   noButton.addEventListener('click', backToMain);
@@ -134,11 +158,15 @@ const restartGame = () => { //when you hit yes button on lose state
   startGame();
   resetBlocks();
   score = 0;
+  speed = 100;
+  rate = 11;
   player.classList.remove('dead')
 };
 const backToMain = () => { //when you hit no button on lose state
   resetBlocks();
-  score = 0
+  score = 0;
+  speed = 100;
+  rate = 11;
   startButton.style.display = 'block';
   title.style.display = 'block';
   finalScore.style.display = 'none';
@@ -149,6 +177,7 @@ const backToMain = () => { //when you hit no button on lose state
   player.classList.remove('dead')
 }
 
+
 //on start up run these functions
 startButton.addEventListener('click', startGame);
-window.requestAnimationFrame(movingPlayer);
+window.requestAnimationFrame(movingParts);
